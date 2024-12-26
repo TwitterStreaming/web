0e4075a7b6c0ac0fd31894fcaf6b2c3464b6101f
muhammad qzih <MuhammadGHQzih@gmail.com>
2024-12-26 21:30:09 +0200

    create web app strcuture

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="main-content">
+      {/* Header  */}
+        <div className="flex">
+          <div className="column-charts">
+            {/* trendChart  */}
+            {/* GaugeChart  */}
+          </div>
+          <div className="column-charts">
+            {/* Map  */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
