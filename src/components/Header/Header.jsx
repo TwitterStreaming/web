2f895944160b81c11702af515989ff7b1e185a27
Matthew <141634353+Matthewkai234@users.noreply.github.com>
2025-01-01 14:48:11 +0300

    Sentiment Analysis State

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,15 +5,18 @@ import profilePic from "../../Assets/money.png";
 import { FaSearch } from "react-icons/fa";
 import { useSearchByKeyword } from "../../context/useSearchByKeyword";
 import { useLocation } from "../../context/useLocation";
+import { useAverageSentiment } from "../../context/useSentimentAnalysis";
 
 const Header = () => {
     const [searchText, setSearchText] = useState("");
     const { data, fetch: fetchSearchData } = useSearchByKeyword();
     const { fetch: fetchLocations } = useLocation();
+    const { fetch: fetchAverageSentiment } = useAverageSentiment();
 
     async function searchKeyword() {
         if (searchText !== "") {
             await fetchSearchData(searchText);
+            await fetchAverageSentiment(searchText);
         }
     }
 
