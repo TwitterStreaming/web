ac851d0d1a77ee4e71c7f7e1e3014d079b7c22bf
Omar <jamlee977@outlook.com>
2025-01-03 16:22:25 +0200

    fix: fixed accessing null

--- src/context/useTotalTweets.js
+++ src/context/useTotalTweets.js
@@ -4,7 +4,7 @@ import { getTotalTweets } from "../data/database_caller";
 export const useTotalTweets = create((set) => ({
     totalTweets: 0,
     setTotalTweets: async () => {
-        const totalTweets = (await getTotalTweets()).total_count;
+        const totalTweets = (await getTotalTweets())?.total_count;
         return set({
             totalTweets,
         });
